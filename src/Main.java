b286b44d795fc9d19e1f04f7548c7a322e156bfc
Anton <anton22582258@gmail.com>
2024-05-27 20:53:22 +0300

    Task ArrayList сделана

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add("E");
+        System.out.println("Массив до реверса :" + arrayList);
+        Collections.reverse(arrayList);
+        System.out.print("Массив после реверса :" + arrayList);
     }
 }
